c7d6b92f5fde35c7d2572fb9cccdc6fb37131c3f
Andrii Simonenko <simonenkoff.a@gmail.com>
2021-02-17 16:50:52 +0200

    Firs commit

---
+++ EtalonToolCheck/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace EtalonToolCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fswEtaalon = new System.IO.FileSystemWatcher();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fswEtaalon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fswEtaalon
+            // 
+            this.fswEtaalon.EnableRaisingEvents = true;
+            this.fswEtaalon.Filter = "*.btw";
+            this.fswEtaalon.SynchronizingObject = this;
+            this.fswEtaalon.Changed += new System.IO.FileSystemEventHandler(this.fswEtaalon_Changed);
+            this.fswEtaalon.Created += new System.IO.FileSystemEventHandler(this.fswEtaalon_Created);
+            this.fswEtaalon.Deleted += new System.IO.FileSystemEventHandler(this.fswEtaalon_Deleted);
+            this.fswEtaalon.Renamed += new System.IO.RenamedEventHandler(this.fswEtaalon_Renamed);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 216);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(13, 14);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(110, 13);
+            this.lblFolder.TabIndex = 1;
+            this.lblFolder.Text = "Отслежуемая папка";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnOpenFolder.Location = new System.Drawing.Point(397, 9);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 2;
+            this.btnOpenFolder.Text = "Открыть папку";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fswEtaalon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fswEtaalon;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.Button btnOpenFolder;
+    }
+}
+
---
+++ EtalonToolCheck/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Diagnostics;
+
+namespace EtalonToolCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fswEtaalon_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            var etalonFolder = ConfigurationManager.AppSettings.Get("EtalonFolder");
+            var zbFolder = ConfigurationManager.AppSettings.Get("ZBFolder");
+            try
+            {
+                MessageBox.Show("Файли які змінювались " + e.FullPath);
+                File.Copy(etalonFolder + "\\", zbFolder + "\\" + e.FullPath + ".btw", true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR" + ex.Message);
+            }
+            
+            textBox1.Text += "Изменен файл " + e.FullPath + "\r\n";
+        }
+
+        private void fswEtaalon_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            textBox1.Text += "Создан файл " + e.FullPath + "\r\n";
+        }
+
+        private void fswEtaalon_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            textBox1.Text += "Удален файл " + e.FullPath + "\r\n";
+        }
+
+        private void fswEtaalon_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            textBox1.Text += "Переименован файл " + e.FullPath + "\r\n";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var etalonFolder = ConfigurationManager.AppSettings.Get("EtalonFolder");
+            var zbFolder = ConfigurationManager.AppSettings.Get("ZBFolder");
+            lblFolder.Text = "Отслежуемая папка: " + etalonFolder;
+            fswEtaalon.Path = etalonFolder;
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            var etalonFolder = ConfigurationManager.AppSettings.Get("EtalonFolder");
+            Process.Start("explorer.exe", etalonFolder);
+        }
+    }
+}
